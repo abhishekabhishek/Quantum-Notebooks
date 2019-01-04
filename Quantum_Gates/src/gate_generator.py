6373e017a9adce6a29d2e61de153fd415620ab00
abhishekabhishek <abhishek@myumanitoba.ca>
2019-01-04 17:27:44 -0600

    Initial simpler gates added

---
+++ Quantum_Gates/src/gate_generator.py
@@ -0,0 +1,6 @@
+#!/home/q/.conda/envs/qdev/bin/python3
+# -*- coding: utf-8 -*-
+
+# Python script to generate a given n-qubit gate using a sequence of standard 
+# gates if such a configuration is possible
+
